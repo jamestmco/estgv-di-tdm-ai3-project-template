aed5fad8b3a5feae9b03ca8056c9f878f09eb1d2
jamestmco <57711005+jamestmco@users.noreply.github.com>
2020-01-30 11:01:44 +0000

    navbar

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -17,14 +17,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MaterialLayoutComponent } from './material-layout/material-layout.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { NavbarComponent } from './nav-bar/nav-bar.component';
 import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MaterialLayoutComponent,
-    NavBarComponent,
+    NavbarComponent,
     ProfileComponent
   ],
   imports: [
